205a1628df29d52d4e1f1d3dcb408cf7feebe735
Dillon Smith <dillsmth@gmail.com>
2021-09-24 21:29:50 -0500

    new commit. better config

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose");
 const compression = require("compression");
 require('dotenv').config(); //dotenv
 
-const PORT = 3000;
+const PORT = process.emv.PORT || 3000;
 
 const app = express();
 
