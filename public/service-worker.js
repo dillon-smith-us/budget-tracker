23a87d37ba32fbda171956d09350d8bc12c680d2
Dillon Smith <dillsmth@gmail.com>
2021-09-24 23:55:27 -0500

    new commit to heroku

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,22 +1,22 @@
 const FILES_TO_CACHE = [
     '/',
-    '/index.html',
-    '/db.js',
-    '/styles.css',
-    '/icons/icon-192x192.png',
-    '/icons/icon-512x512.png',
-    '/manifest.webmanifest',
+    'index.html',
+    'db.js',
+    'styles.css',
+    'icons/icon-192x192.png',
+    'icons/icon-512x512.png',
+    'manifest.webmanifest',
     'https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css',
     'https://cdn.jsdelivr.net/npm/chart.js@2.8.0'
 ];
 
-const CACHE_NAME = 'static-cache-v13';
-const DATA_CACHE_NAME = 'data-cache-v8';
+const CACHE_NAME = 'static-cache-v2';
+const DATA_CACHE_NAME = 'data-cache-v1';
 
 // INSTALL
-self.addEventListener('install', (e) => {
-    e.waitUntil(
-        caches.open(CACHE_NAME).then((cache)=> {
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(cache=> {
             return cache.addAll(FILES_TO_CACHE);
         })
     );
@@ -24,48 +24,51 @@ self.addEventListener('install', (e) => {
 });
 
 // ACTIVATE
-self.addEventListener('activate', (e) => {
+self.addEventListener('activate', event => {
     //remove old caches
-    e.waitUntil(
-        caches.keys().then((keyList) => {
+    event.waitUntil(
+        caches.keys().then(keyList => {
             return Promise.all(
                 keyList.map((key) => {
                     if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
+                        console.log('deleting previous cache', key)
                         return caches.delete(key);
                     }
                 })
             )
         })
-    );
-    self.clients.claim();
+    )
+    .then((self.clients.claim()));
 });
 
-self.addEventListener('fetch', (e) => {
+self.addEventListener('fetch', (event) => {
     // cache succesful GET request to the API
-    if (e.req.url.includes('/api/') && e.req.method === 'GET') {
-        e.respondWith(
+    if (event.request.url.includes('/api/')) {
+        event.respondWith(
             caches
                 .open(DATA_CACHE_NAME)
-                .then((cache) => {
-                    return fetch(e.req)
-                    .then((res) => {
+                .then(cache => {
+                    return fetch(event.request)
+                    .then(response => {
                         // if response is good, clone and store it in cache
-                        if (res.status === 200) {
-                            cache.put(e.req, res.clone());
+                        if (response.status === 200) {
+                            cache.put(event.request.url, response.clone());
                         }
-                        return res;
+                        return response;
                     })
-                    .catch(() => {
-                        return cache.match(e.req);
+                    .catch((error) => {
+                        return cache.match(event.request);
                     });
                 })
-                .catch((err) => console.log(err))
+                .catch(err => console.log(err))
         );
         return;
     }
-    e.respondWith(
-        caches.match(e.req).then((res)=> {
-            return res || fetch(e.req);
+    event.respondWith(
+        caches.open(CACHE_NAME).then(cache => {
+            return cache.match(event.request).then(response=> {
+                return response || fetch(event.request);
+            }) 
         })
     )
 })
