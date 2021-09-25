1d207e767ccd7bb0dbbbdb6491b315aaa68335a1
Dillon Smith <dillsmth@gmail.com>
2021-09-25 00:11:50 -0500

    index.js added to cache

--- public/service-worker.js
+++ public/service-worker.js
@@ -3,6 +3,7 @@ const FILES_TO_CACHE = [
     'index.html',
     'db.js',
     'styles.css',
+    'index.js',
     'icons/icon-192x192.png',
     'icons/icon-512x512.png',
     'manifest.webmanifest',
